72b12e13e9c5d868678e299afd52cbfb64393bbd
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-09 22:30:45 +0200

    Section 1 and 2 - I'm created my first app in C#

--- FirstAppCsharp/Program.cs
+++ FirstAppCsharp/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello World!!!");
+﻿Console.WriteLine("To jest moja pierwsza aplikacja w C#!");
+Console.Write("Podaj swoję imię: ");
+
+string name = Console.ReadLine();
+Console.WriteLine("Witaj, " + name + "!");
+
+Console.WriteLine();
+
+Console.Write("Podaj swój ulubiony kolor: ");
+string color = Console.ReadLine();
+Console.WriteLine("Twój ulubiony kolor to: " + color + ".");
