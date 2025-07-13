8ee7dc432d4a44d59f4a4ab96ca3d35b0306ea0a
Dawid Płatek <dawidplatek.eu.pl@gmail.com>
2025-07-13 23:02:43 +0200

    Section 3 - using basic C# building blocks

--- CsharpSyntax/Program.cs
+++ CsharpSyntax/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Wypisany tekst musi znajdować się w podwójnych cudzysłowach.");
+
+Console.WriteLine("Podaj swój ulubiony kolor: ");
+string color = Console.ReadLine();
+
+//string color2 = Console.ReadLine();
+//string Color2 = Console.ReadLine();
+//string color_2 = Console.ReadLine();
+//string Color_2 = Console.ReadLine();
+
+// string 2Color = Console.ReadLine(); // Błąd: nazwa zmiennej nie może zaczynać się od cyfry
+
+/* Komentarz
+ wieloliniowy
+*/
